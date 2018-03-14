92e27502bf2f8c6365e15db11025281b745e4207
januari12 <januari.yanti@student.untan.ac.id>
2018-03-14 13:44:10 +0700

    Latihan 1, Halaman Perdana

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//ini adalah komentar saja
+// bagaimana bila
+// engkau wahai komentar
+// berpanjang lebar dalam berkata
+	echo "Fizri Septa Yunda";
+	echo "H1101161025";
+
+	echo "Hello world!";
+	echo "Hello world!";
+	echo "Hello world!";
+	echo "Hello world!";
+	echo "Hello world!";
+	
+	$aaa=23;
+	echo "berat badan saya adalah" .$aaa. "kg
+
+		";
+
+?>
